783224a493f26ad5dc5048bc871dc5d64d356a87
Adlai M.B <adlaimb@gmail.com>
2025-07-30 17:29:01 -0700

    add width sizing to List component

--- src/app/_components/List.js
+++ src/app/_components/List.js
@@ -2,7 +2,7 @@ import { Fragment } from "react";
 
 export default function List({ children, items, renderItem }) {
   return (
-    <div className="bg-gray-500">
+    <div className="bg-gray-500 w-full">
       {children}
       <ul className="flex flex-col items-center gap-4 px-[4%] py-4">
         {items.map((item, index) => (
