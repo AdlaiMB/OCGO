2d7bebedb83ccdbe9ddcb9526b5fb8ef47d3e7d4
Adlai M.B <adlaimb@gmail.com>
2025-09-22 17:11:36 -0700

    add responsiveness to the session navbar

--- src/app/(session)/_components/NavBar.js
+++ src/app/(session)/_components/NavBar.js
@@ -1,7 +1,7 @@
 export default async function NavBar() {
   return (
-    <div id="NavBar" className="navbar px-10 py-4">
-      <div className="text-4xl font-bold">
+    <div id="NavBar" className="navbar px-6 md:px-10 py-4">
+      <div className="text-3xl md:text-4xl font-bold">
         <span className="text-[#E8963A]">OC</span>
         <span className="text-[#4B8E5A]">GO</span>
       </div>
